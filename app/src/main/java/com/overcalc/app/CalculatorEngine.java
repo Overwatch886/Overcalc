b21e1dacef1850d740eea8838f557aa64732d4cb
Olawuyi Israel Mobolaji <olawuyiisrael2@gmail.com>
2026-05-22 10:14:23 +0100

    First commit for the overcalc project, featuring the inteface, the The calculator engine and the main activity connecting the interface to the calculator engine. The calculator is still in a two-input state, with the first input in the first text box and the second input in the second text box.

---
+++ app/src/main/java/com/overcalc/app/CalculatorEngine.java
@@ -0,0 +1,29 @@
+package com.overcalc.app;
+
+public class CalculatorEngine {
+    public double add(double num1, double num2){
+        return num1 + num2;
+    }
+    public double subtract(double num1, double num2){
+        return num1 - num2;
+    }
+    public double multiply(double num1, double num2){
+        return num1 * num2;
+    }
+    public double divide(double num1, double num2){
+        if (num2 == 0.0){
+            return Double.NaN;
+        }
+        else{
+            return num1/num2;
+        }
+    }
+    public long factorial(int n){
+        if (n == 1 || n == 0){
+            return 1;
+        }
+        else {
+            return n * factorial(n - 1);
+        }
+    }
+}
---
+++ app/src/main/java/com/overcalc/app/MainActivity.java
@@ -0,0 +1,101 @@
+package com.overcalc.app;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    EditText etNum1, etNum2;
+    Button btnAdd, btnSubtract, btnMultiply, btnDivide, btnFactorial;
+    TextView tvResult;
+    CalculatorEngine engine;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        engine = new CalculatorEngine();
+        etNum1 = (EditText) findViewById(R.id.etNum1);
+        etNum2 = (EditText) findViewById(R.id.etNum2);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnDivide = (Button) findViewById(R.id.btnDivide);
+        btnSubtract = (Button) findViewById(R.id.btnSubtract);
+        btnMultiply = (Button) findViewById(R.id.btnMultiply);
+        btnFactorial = (Button) findViewById(R.id.btnFactorial);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                try {
+                    double num1 = Double.parseDouble(etNum1.getText().toString());
+                    double num2 = Double.parseDouble(etNum2.getText().toString());
+                    double result = engine.add(num1, num2);
+                    tvResult.setText(String.valueOf(result));
+                }
+                catch (NumberFormatException e){
+                    tvResult.setText("Enter Valid Numbers!");
+                }
+            }
+        });
+        btnSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                try {
+                    double num1 = Double.parseDouble(etNum1.getText().toString());
+                    double num2 = Double.parseDouble(etNum2.getText().toString());
+                    double result = engine.subtract(num1, num2);
+                    tvResult.setText(String.valueOf(result));
+                }
+                catch (NumberFormatException e){
+                    tvResult.setText("Enter Valid Numbers!");
+                }
+            }
+        });
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                try {
+                    double num1 = Double.parseDouble(etNum1.getText().toString());
+                    double num2 = Double.parseDouble(etNum2.getText().toString());
+                    double result = engine.multiply(num1, num2);
+                    tvResult.setText(String.valueOf(result));
+                }
+                catch (NumberFormatException e){
+                    tvResult.setText("Enter Valid Numbers!");
+                }
+            }
+        });
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                try {
+                    double num1 = Double.parseDouble(etNum1.getText().toString());
+                    double num2 = Double.parseDouble(etNum2.getText().toString());
+                    double result = engine.divide(num1, num2);
+                    tvResult.setText(String.valueOf(result));
+                }
+                catch (NumberFormatException e){
+                    tvResult.setText("Enter Valid Numbers!");
+                }
+            }
+        });
+        btnFactorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                try {
+                    int num1 = Integer.parseInt(etNum1.getText().toString());
+                    long result = engine.factorial(num1);
+                    tvResult.setText(String.valueOf(result));
+                }
+                catch (NumberFormatException e){
+                    tvResult.setText("Enter Valid Numbers!");
+                }
+            }
+        });
+
+    }
+}
